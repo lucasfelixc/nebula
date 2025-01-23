f578cdd1290905521278adadd099c2662c8adc0e
lucasfelixc <lucasfelixdev@gmail.com>
2025-01-23 09:13:18 -0300

    Remove unnecessary files and implement child test on create node function

--- src/core/createNode.test.ts
+++ src/core/createNode.test.ts
@@ -2,8 +2,8 @@ import { describe, it, expect } from 'vitest';
 import { createNode } from './createNode';
 
 describe('createNode', () => {
-    it('should create a node with type and props', () => {
-        const node = createNode('div', { id: 'app' }, 'Hello, world;');
+    it('should create a node with text child', () => {
+        const node = createNode('div', { id: 'app' }, 'Hello, world');
 
         expect(node).toEqual({
             type: 'div',
@@ -12,8 +12,33 @@ describe('createNode', () => {
                 children: [{
                     type: 'TEXT_ELEMENT',
                     props: {
-                        nodeValue: 'Hello, world;',
+                        nodeValue: 'Hello, world',
                         children: [],
+                    },
+                }],
+            },
+        });
+    });
+
+    it('should create a node with element child', () => {
+        const child = createNode('span', { id: 'child' }, 'Hello, child');
+        const node = createNode('div', { id: 'app' }, child);
+
+        expect(node).toEqual({
+            type: 'div',
+            props: {
+                id: 'app',
+                children: [{
+                    type: 'span',
+                    props: {
+                        id: 'child',
+                        children: [{
+                            type: 'TEXT_ELEMENT',
+                            props: {
+                                nodeValue: 'Hello, child',
+                                children: [],
+                            },
+                        }],
                     },
                 }],
             },
